bb0bd29a9bd29e68cd765c71bdaf9961240b08f1
Kevin Choy <kgchoy@sfu.ca>
2015-08-01 00:03:39 -0700

    testing projects section on main page

---
+++ kirby/site/snippets/mainPageProjects.php
@@ -0,0 +1,55 @@
+<span class="anchor-link" id="featured-work"></span>
+
+<h2>Featured Work</h2>
+
+<div class="grid">
+    <div class="main-page-work-grid">
+        <div class="col-1of3-work-main">
+            
+
+            <div class="col-1of3-work-figure">
+                <div class="main-work-listing-figure">
+                    <figure>
+                        <a href="bccampus.html"><img src="img/bcc-website-screenshot-cropped.jpg" alt="BCcampus website screenshot"></a>
+                    </figure>
+                </div>
+            </div>
+            
+            <div class="col-2of3-work-description">
+                <div class="work-description">
+                    <h3><a href="bccampus.html">BCcampus.ca Usability Testing</a></h3>
+                    <em>UX research &#149; usability testing</em>
+                    <p>
+                        I created and facilitated a usability test for the BCcampus.ca website to find out whether the audience could perform typical tasks on the website easily, and to discover usability problems.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="main-page-work-grid">
+        <div class="col-1of3-work-main">
+            
+            
+            <div class="col-1of3-work-figure">
+                <div class="main-work-listing-figure">
+                    <figure>
+                        <a href="notif.html"><img src="img/notif-thumb.png" alt="Notif use case screenshot"></a>
+                    </figure>
+                </div>
+            </div>
+
+            <div class="col-2of3-work-description">
+                <div class="work-description">
+                    <h3><a href="notif.html">Notif</a></h3>
+                    <em>UX research &amp; development &#149; usability testing &#149; programming</em>
+                    <p>
+                        Prototyping a concept mapping tool targeted towards the academic teaching and learning domain. The design and functionality of our tool was heavily use-case and scenario-driven.
+                    </p>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div> <!-- ./grid (3-column) -->
--- kirby/site/templates/home.php
+++ kirby/site/templates/home.php
@@ -1,6 +1,30 @@
 <?php snippet('header') ?>
 
-  <main class="main" role="main">
+  <!-- <main class="main" role="main"> -->
+
+    <div id="page-container">
+        <div class="grid">
+            <div class="col-3of3">
+                <article id="first-article" class="article-container">
+                    <div id="banner-text">
+                        <p>
+                            <?php echo $page->main_banner_text() ?>
+                        </p>
+
+                        <a class="button-nav partial-width-button" href="about.html">About Me&nbsp;&nbsp;&rsaquo;&rsaquo;</a>
+
+                    </div>
+                </article>
+
+                <?php snippet('mainPageProjects') ?>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
 
     <div class="text">
       <h1><?php echo $page->title()->html() ?></h1>
@@ -11,6 +35,6 @@
 
     <?php snippet('projects') ?>
 
-  </main>
+  <!-- </main> -->
 
-<?php snippet('footer') ?>
+<!-- <?php snippet('footer') ?> -->
